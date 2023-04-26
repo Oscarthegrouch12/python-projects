3b23ad5ed6df794b234618b53590a8535f486c97
Oscar Dalkin <oscardalkin@gmail.com>
2023-04-26 14:35:20 +1000

    simple 7 line madlib game

---
+++ PycharmProjects/pythonProject/mad_libs_game.py
@@ -0,0 +1,27 @@
+
+
+print("Mad-Lib game!")
+adjective1 = input("Enter an adjective:")
+adjective2 = input("Enter an adjective:")
+bird = input("Enter a type of bird:")
+room = input("Enter a room in a house:")
+verb_past = input("Enter a past tense verb:")
+verb = input("Enter a verb:")
+relative = input("Enter a relatives name:")
+noun1 = input("Enter a noun:")
+liquid = input("Enter a liquid:")
+verb_ing1 = input("Enter a verb ending in -ing:")
+body_part = input("Enter a part of the body(plural):")
+plural_noun = input("Enter a plural noun:")
+verb_ing2 = input("Enter a verb ending in -ing:")
+noun2 = input("Enter a noun:")
+
+print("It was a " + adjective1 + ", cold November day.")
+print("I woke up to the " + adjective2 + " smell of " + bird + " roasting in the " + room + " downstairs. ")
+print("I " + verb_past + " down the stairs to see if I could help " + verb + " the dinner.")
+print("My mum said, 'See if " + relative + " needs a fresh " + noun1 + "'.")
+print("So I carried a tray of glasses full of " + liquid + " into the " + verb_ing1 + " room.")
+print("when I got there, I couldn't believe my " + body_part + "!")
+print("There were " + plural_noun + " " + verb_ing2 + " on the " + noun2 + "!")
+
+
